85c00cce7b4701f8d9e601104b93b865fa536ea5
Mahan-Sahebdel <mahansahebdel1@gmail.com>
2024-07-08 01:39:06 +0330

    conflict detection

--- js/select_course.js
+++ js/select_course.js
@@ -1,21 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const selectedCourses = [];
-  const selectedCoursesTable = document.getElementById(
-    "selected-courses-table"
-  );
+const selectedCourses = [];
+const selectedCoursesTable = document.getElementById("selected-courses-table");
 
-  // Add event listener to each "انتخاب" button
-  const selectButtons = document.querySelectorAll("table button.select-button");
-  selectButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      const row = this.closest("tr");
-      const courseName = row.querySelector("td:nth-child(1)").textContent;
-      const professorName = row.querySelector("td:nth-child(2)").textContent;
-      const classDay = row.querySelector("td:nth-child(3)").textContent;
-      const classTime = row.querySelector("td:nth-child(4)").textContent;
+// Add event listener to each "انتخاب" button
+const selectButtons = document.querySelectorAll("table button.select-button");
+selectButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const row = this.closest("tr");
+    const courseName = row.querySelector("td:nth-child(1)").textContent;
+    const professorName = row.querySelector("td:nth-child(2)").textContent;
+    const classDay = row.querySelector("td:nth-child(3)").textContent;
+    const classTime = row.querySelector("td:nth-child(4)").textContent;
 
-      // Check if the course is already selected
-      if (!isSelectedCourse(courseName)) {
+    // Check if the course is already selected or conflicts with another course
+    if (!isSelectedCourse(courseName)) {
+      if (!isConflict(classDay, classTime)) {
         // Add the course to the selectedCourses array
         selectedCourses.push({
           courseName: courseName,
@@ -27,12 +25,12 @@ document.addEventListener("DOMContentLoaded", function () {
         // Create a new row in the selected courses table
         const newRow = document.createElement("tr");
         newRow.innerHTML = `
-            <td>${courseName}</td>
-            <td>${professorName}</td>
-            <td>${classDay}</td>
-            <td>${classTime}</td>
-            <td><button class="btn btn-danger btn-sm remove-button">حذف</button></td>
-          `;
+          <td>${courseName}</td>
+          <td>${professorName}</td>
+          <td>${classDay}</td>
+          <td>${classTime}</td>
+          <td><button class="btn btn-danger btn-sm remove-button">حذف</button></td>
+        `;
         selectedCoursesTable.appendChild(newRow);
 
         // Add event listener to the "حذف" button
@@ -42,23 +40,31 @@ document.addEventListener("DOMContentLoaded", function () {
           newRow.remove();
         });
       } else {
-        alert("این درس قبلاً انتخاب شده است.");
+        alert("درس مورد درخواست با دروس انتخاب شده تداخل دارد.");
       }
-    });
-  });
-
-  function isSelectedCourse(courseName) {
-    return selectedCourses.some(function (course) {
-      return course.courseName === courseName;
-    });
-  }
-
-  function removeSelectedCourse(courseName) {
-    const index = selectedCourses.findIndex(function (course) {
-      return course.courseName === courseName;
-    });
-    if (index !== -1) {
-      selectedCourses.splice(index, 1);
+    } else {
+      alert("این درس قبلاً انتخاب شده است.");
     }
-  }
+  });
 });
+
+function isSelectedCourse(courseName) {
+  return selectedCourses.some(function (course) {
+    return course.courseName === courseName;
+  });
+}
+
+function isConflict(classDay, classTime) {
+  return selectedCourses.some(function (course) {
+    return course.classDay === classDay && course.classTime === classTime;
+  });
+}
+
+function removeSelectedCourse(courseName) {
+  const index = selectedCourses.findIndex(function (course) {
+    return course.courseName === courseName;
+  });
+  if (index !== -1) {
+    selectedCourses.splice(index, 1);
+  }
+}
